9da59dd8adfab17751ce6e902b5bfdf36e77280e
briup-teacher <1373501393@qq.com>
2022-10-14 09:05:50 +0800

    springmvc-day05

---
+++ springmvc-day05/src/main/java/com/briup/bean/Student.java
@@ -0,0 +1,28 @@
+package com.briup.bean;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+/**
+ * @Author lining
+ * @Date 2022/10/12
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Student {
+    private int id;
+    private String name;
+    //@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:dd")//处理表单请求参数时间转换
+    //@JsonFormat(pattern = "yyyy/MM/dd",timezone = "GMT+8")
+    public String test(){
+        System.out.println("spEl表达式调用方法");
+        return "tom";
+    }
+}
